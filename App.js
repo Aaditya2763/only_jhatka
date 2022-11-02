2a65255e7436c1db59c72fce2300b79dfae29119
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-11-02 18:19:55 +0530

    add spalsh Screen

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <View style={styles.Home}>
+      <Text style={styles.text}>HomeScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Home: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    color: 'black',
+  },
+});
+
+export default App;
