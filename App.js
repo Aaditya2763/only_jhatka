03053bcde0465cd7aea05a921100443de4a02acd
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-11-10 14:41:20 +0530

    remove splash screen

--- App.js
+++ App.js
@@ -6,18 +6,13 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
-import SplashScreen from 'react-native-splash-screen';
+import React from 'react';
 import SetLocationScreen from './src/screens/setLocationScreen';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
 
 const App = () => {
-  useEffect(() => {
-    // splashScreen hide method
-    SplashScreen.hide();
-  }, []);
   return <SetLocationScreen />;
 };
 
