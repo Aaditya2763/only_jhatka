18d4e1e001d9b89dcd4271ed2f9f3e7e99287ca6
lekhrajsaini0 <singhaditya2763@gmail.com>
2022-11-14 09:43:29 +0530

    added splash screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { enableScreens } from 'react-native-screens';
 
 //importing Screens
- import SplashScreen from './src/screens/splashScreen';
+import SplashScreen from './src/screens/splashScreen';
 import SetLocationScreen from './src/screens/setLocationScreen';
 
 //calling stack navigator
@@ -15,35 +15,35 @@ enableScreens(true);
 
 const App = () => {
 
-  const [ShowSplashScreen ,setShowSplashScreen]=useState(true);
-  
-  useEffect(()=>{
-   setTimeout(()=>{
-setShowSplashScreen(false);
-   },5000)
- 
-  },[]);
+  const [ShowSplashScreen, setShowSplashScreen] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplashScreen(false);
+    }, 5000)
+
+  }, []);
 
   return (
-   <NavigationContainer>
-    <Stack.Navigator>
-      {/* <SplashScreen/> */}
-      {/* <Home/> */}
-      {ShowSplashScreen ?<Stack.Screen
-      name="splashScreen"
-      component={SplashScreen} 
-      options={{headerShown:false}}
-    />
-    : null
-    }
-    <Stack.Screen
-      name="setLocationScreen"
-      component={SetLocationScreen} 
-      options={{headerShown:false}}
-    />
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <SplashScreen/> */}
+        {/* <Home/> */}
+        {ShowSplashScreen ? <Stack.Screen
+          name="splashScreen"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+          : null
+        }
+        <Stack.Screen
+          name="setLocationScreen"
+          component={SetLocationScreen}
+          options={{ headerShown: false }}
+        />
 
       </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   );
 };
 
